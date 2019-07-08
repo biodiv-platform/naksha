71cfcfe1f92ddfda7f9645032d29b27dcdbae26f
Harsh Zalavadiya <harsh@strandls.com>
2019-07-08 11:53:35 +0530

    fix(layers): endpoint path fix

--- src/main/java/com/strandls/naksha/common/ApiConstants.java
+++ src/main/java/com/strandls/naksha/common/ApiConstants.java
@@ -4,7 +4,7 @@ public class ApiConstants {
 
 	// LayersController
 	public static final String UPLOADSHP = "/uploadshp";
-	public static final String LAYERS = "/layers";
+	public static final String LAYERS = "/layer";
 	public static final String GEOSERVER = "/geoserver";
 
 }
